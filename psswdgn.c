b838060ec1a3a0bbcdfce24fafd02624a73ade77
qywok <59132829+belajarqywok@users.noreply.github.com>
2021-09-28 13:07:07 +0700

    Add files via upload

---
+++ psswdgn.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if((strcmp(argv[1],"2") == 0)){
+        printf("You entered the number 1.");
+    } else {
+        printf("You entered a different number.");
+    }
+
+    return 0;
+}
